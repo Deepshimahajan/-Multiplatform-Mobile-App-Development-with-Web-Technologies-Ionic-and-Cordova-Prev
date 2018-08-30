d6f3e0ed64d528a339c3e75865c5c40d7e932390
Deepshi Mahajan <dmahajan@scu.edu>
2018-08-30 08:57:35 -0700

    FAB

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { LeaderProvider } from '../../providers/leader/leader';
+import { Leader } from '../../shared/leader';
 
 /**
  * Generated class for the AboutPage page.
@@ -15,11 +17,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  leaders: Leader[];
+  leaderErrMess: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private leaderservice: LeaderProvider, 
+    @Inject('BaseURL') private BaseURL) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AboutPage');
+  }
+  ngOnInit() {
+    this.leaderservice.getLeaders()
+    .subscribe(leaders => this.leaders = leaders,
+      errmess => this.leaderErrMess = <any>errmess.message);
   }
 
 }
